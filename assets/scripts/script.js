bbf2b29b937a00da5a1636e123bb97535c3a0be2
shajanjp <shajanjp@gmail.com>
2020-11-12 22:34:11 +0530

    fix: adds data json fetch

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,16 @@
 const textContainer = document.getElementById("text-container");
 
+const appsList = [
+  {
+    title: "",
+    fileName: "vscode.json",
+  },
+  {
+    title: "",
+    fileName: "sublime-text.json",
+  },
+];
+
 document.addEventListener("keydown", function (event) {
   event.preventDefault();
   event.stopPropagation();
@@ -9,3 +20,19 @@ document.addEventListener("keydown", function (event) {
 function writeToDocument(text) {
   textContainer.innerHTML = text;
 }
+
+async function fetchData(path) {
+  const response = await fetch(path);
+  return await response.json();
+}
+
+const promises = appsList.map((app) => {
+  return fetchData(`data/${app.fileName}`);
+});
+
+(async () => {
+  let keymaps = [];
+  (await Promise.all(promises)).map((keymap) => {
+    keymaps = keymaps.concat(keymap);
+  });
+})();
